9d82ca6de32b84f7602c7c2ecaebd9d7965905e9
artyom2509 <bicepssapiens228@gmail.com>
2021-04-05 18:51:06 +0600

    stripe connection implementing compleated

--- src/sections/User/components/UserProfile/index.tsx
+++ src/sections/User/components/UserProfile/index.tsx
@@ -1,38 +1,117 @@
 import { User as UserData } from '../../../../lib/graphql/queries/User/__generated__/User';
-import { Avatar, Button, Card, Divider, Typography } from 'antd';
+import { Avatar, Button, Card, Divider, Tag, Typography } from 'antd';
+import { useMutation } from '@apollo/react-hooks';
+import {
+	displayErrorMessage,
+	displaySuccessNotification,
+	formatListingPrice,
+} from '../../../../lib/utils';
+import { Fragment } from 'react';
+import { Viewer } from '../../../../lib/types';
+import { DISCONNECT_STRIPE } from '../../../../lib/graphql/mutations/DisconnectStripe';
+import { DisconnectStripe as DisconnectStripeData } from '../../../../lib/graphql/mutations/DisconnectStripe/__generated__/DisconnectStripe';
 
 const { Paragraph, Text, Title } = Typography;
 
 interface Props {
 	user: UserData['user'];
+	viewer: Viewer;
 	viewerIsUser: boolean;
+	setViewer: (viewer: Viewer) => void;
+	handleUserRefetch: () => void;
 }
 
-export const UserProfile = ({ user, viewerIsUser }: Props) => {
+const stripeAuthUrl = `https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_S_CLIENT_ID}&scope=read_write`;
+
+export const UserProfile = ({
+	user,
+	viewer,
+	viewerIsUser,
+	setViewer,
+	handleUserRefetch,
+}: Props) => {
+	const [disconnectStripe, { loading }] = useMutation<DisconnectStripeData>(
+		DISCONNECT_STRIPE,
+		{
+			onCompleted: (data) => {
+				if (data && data.disconnectStripe) {
+					setViewer({ ...viewer, hasWallet: data.disconnectStripe.hasWallet });
+					displaySuccessNotification(
+						"You've successfully disconnected from Stripe!",
+						"You'll have to reconnect with Stripe to continue to create listings."
+					);
+					handleUserRefetch();
+				}
+			},
+			onError: () => {
+				displayErrorMessage(
+					"Sorry! We weren't able to disconnect you from Stripe. Please try again later!"
+				);
+			},
+		}
+	);
+
+	const redirectToStripe = () => {
+		window.location.href = stripeAuthUrl;
+	};
+
+	const additionalDetails = user.hasWallet ? (
+		<Fragment>
+			<Paragraph>
+				<Tag color="green">Stripe Registered</Tag>
+			</Paragraph>
+			<Paragraph>
+				Income Earned:{' '}
+				<Text strong>
+					{user.income ? formatListingPrice(user.income) : `$0`}
+				</Text>
+			</Paragraph>
+			<Button
+				type="primary"
+				className="user-profile__details-cta"
+				loading={loading}
+				onClick={() => disconnectStripe()}>
+				Disconnect Stripe
+			</Button>
+			<Paragraph type="secondary">
+				By disconnecting, you won't be able to receive{' '}
+				<Text strong>any further payments</Text>. This will prevent users from
+				booking listings that you might have already created.
+			</Paragraph>
+		</Fragment>
+	) : (
+		<Fragment>
+			<Paragraph>
+				Interested in becoming a TinyHouse host? Register with your Stripe
+				account!
+			</Paragraph>
+			<Button
+				type="primary"
+				className="user-profile__details-cta"
+				onClick={redirectToStripe}>
+				Connect with Stripe
+			</Button>
+			<Paragraph type="secondary">
+				TinyHouse uses{' '}
+				<a
+					href="https://stripe.com/en-US/connect"
+					target="_blank"
+					rel="noopener noreferrer">
+					Stripe
+				</a>{' '}
+				to help transfer your earnings in a secure and truster manner.
+			</Paragraph>
+		</Fragment>
+	);
+
 	const additionalDetailsSection = viewerIsUser ? (
-		<>
+		<Fragment>
 			<Divider />
 			<div className="user-profile__details">
 				<Title level={4}>Additional Details</Title>
-				<Paragraph>
-					Interested in becoming a TynyHouse host? Register with your Stripe
-					account!
-				</Paragraph>
-				<Button type="primary" className="user-profile__details-cta">
-					Connect with Stripe
-				</Button>
-				<Paragraph type="secondary">
-					TynyHouse uses{' '}
-					<a
-						href="https://stripe.com/en-US/connect"
-						target="_blank"
-						rel="noopener noreferrer">
-						Stripe
-					</a>{' '}
-					to help transfer your earnings in a secure and truster manner.
-				</Paragraph>
+				{additionalDetails}
 			</div>
-		</>
+		</Fragment>
 	) : null;
 
 	return (
